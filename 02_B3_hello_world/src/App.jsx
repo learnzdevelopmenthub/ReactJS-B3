f5d424f2a8480a20e8ffef4b946db9281e74a259
learnzdevelopmenthub <learnzdevelopmenthub@gmail.com>
2025-03-19 21:29:08 +0530

    added hello world app with basic component

---
+++ 02_B3_hello_world/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Greetings from './Greetings'
+
+function App() {
+  console.log("app component.")
+  return (
+    <div>
+      <h1>Main Component</h1>
+      <Greetings />
+    </div>
+  )
+}
+
+export default App
---
+++ 02_B3_hello_world/src/Greetings.jsx
@@ -0,0 +1,11 @@
+
+const Greetings = () => {
+    return (
+        <>
+            <h1>Hello</h1>
+            <h2>Test</h2>
+        </>
+    )
+}
+
+export default Greetings
